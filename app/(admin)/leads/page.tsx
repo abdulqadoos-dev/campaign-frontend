fd0c62f5dd5595b481e55bf14764cda2c83d2853
abdul qadoos <abdulqadoos.work@gmail.com>
2024-01-15 00:20:00 +0500

    seprate filter component

--- app/(admin)/leads/page.tsx
+++ app/(admin)/leads/page.tsx
@@ -10,7 +10,7 @@ import Heading from "@ui/heading";
 import AddIcon from "@icons/add.svg";
 import EditIcon from "@icons/edit.svg";
 import RightIcon from "@icons/right.svg";
-import UserImage from '@images/person.jpeg';
+
 import defaultUser from '@icons/leads.svg';
 
 import Form from './form';
@@ -18,14 +18,18 @@ import Header from "@ui/header";
 
 import { useEffect, useState } from 'react';
 import { searchLeads } from './actions';
+
 import Input from '@/app/ui/input';
 import Select from '@/app/ui/select';
 import { statusOptions } from '@/app/constants';
-import Action from '@/app/ui/action';
-
+import UserImage from '@images/person.jpeg';
 import leftIcon from '@icons/chevron-left.svg';
 import rightIcon from '@icons/chevron-right.svg';
+
+import Action from '@/app/ui/action';
 import NoRecord from '@/app/ui/noRecord';
+
+import Filters from '@/app/ui/filters';
 
 interface PropsObject {
 
@@ -125,43 +129,7 @@ const Leads: React.FC<PropsObject> = () => {
         }} />
       </Header>
 
-      <section className="flex items-center justify-between">
-
-        <div className="flex items-center gap-3">
-          <span>Filters:</span>
-
-          <Input name='search' type='serach' value={filters.query} placeholder='search..'
-            onChange={(e: any) => {
-              let newFilters = { ...filters, query: e.target.value, skip: 0 }
-              setFilters(newFilters)
-            }}
-          />
-
-          <Select palceholder="All" name='status' className="w-36" selected={filters.status} options={statusOptions}
-            onChange={(e: any) => {
-              let newFilters = { ...filters, status: e.target.value, skip: 0 }
-              setFilters(newFilters)
-            }}
-          />
-        </div>
-
-        <div className="flex items-center text-sm text-zinc-400 gap-2">
-
-          {filters.skip > 0 ? <Action icon={leftIcon} onClick={() => {
-            let newFilters = { ...filters, skip: filters.skip - 20 }
-            setFilters(newFilters)
-          }} /> : <Action icon={leftIcon} />}
-
-          <span>{`${(filters.take + filters.skip) < leadsCount ? filters.skip + " - " : ""} ${(filters.take + filters.skip) < leadsCount ? (filters.take + filters.skip) : leadsCount} of ${leadsCount}`}</span>
-
-          {(filters.take + filters.skip) < leadsCount ? <Action icon={rightIcon} onClick={() => {
-            let newFilters = { ...filters, skip: filters.skip + 20 }
-            setFilters(newFilters)
-          }} /> : <Action icon={rightIcon} />}
-
-        </div>
-
-      </section>
+      <Filters filters={filters} setFilters={setFilters} count={leadsCount} />
 
 
       {leads?.length ? leads.map((lead: any, index: number) => (
--- app/ui/filters.tsx
+++ app/ui/filters.tsx
@@ -1,2 +1,58 @@
+import Input from '@/app/ui/input';
+import Select from '@/app/ui/select';
+import { statusOptions } from '@/app/constants';
+import Action from '@/app/ui/action';
+import leftIcon from '@icons/chevron-left.svg';
+import rightIcon from '@icons/chevron-right.svg';
+interface Props {
+
+  filters: { query: string, status: string, skip: number, take: number };
+  setFilters: any;
+  count: any;
+
+}
 
 
+
+const Filters: React.FC<Props> = ({ filters, setFilters, count }) => {
+  return <section className="flex items-center justify-between">
+
+    <div className="flex items-center gap-3">
+      <span>Filters:</span>
+
+      <Input name='search' type='serach' value={filters.query} placeholder='search..'
+        onChange={(e: any) => {
+          let newFilters = { ...filters, query: e.target.value, skip: 0 }
+          setFilters(newFilters)
+        }}
+      />
+
+      <Select palceholder="All" name='status' className="w-36" selected={filters.status} options={statusOptions}
+        onChange={(e: any) => {
+          let newFilters = { ...filters, status: e.target.value, skip: 0 }
+          setFilters(newFilters)
+        }}
+      />
+    </div>
+
+    <div className="flex items-center text-sm text-zinc-400 gap-2">
+
+      {filters.skip > 0 ? <Action icon={leftIcon} onClick={() => {
+        let newFilters = { ...filters, skip: filters.skip - 20 }
+        setFilters(newFilters)
+      }} /> : <Action icon={leftIcon} />}
+
+      <span>{`${(filters.take + filters.skip) < count ? filters.skip + " - " : ""} ${(filters.take + filters.skip) < count ? (filters.take + filters.skip) : count} of ${count}`}</span>
+
+      {(filters.take + filters.skip) < count ? <Action icon={rightIcon} onClick={() => {
+        let newFilters = { ...filters, skip: filters.skip + 20 }
+        setFilters(newFilters)
+      }} /> : <Action icon={rightIcon} />}
+
+    </div>
+
+  </section>
+
+}
+
+export default Filters
